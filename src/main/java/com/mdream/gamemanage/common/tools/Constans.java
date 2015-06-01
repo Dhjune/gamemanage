6c2e6b27c4ecbc39cde5d92333064483220c4a6b
Dhjune; <505416245@qq.com;>
2015-06-01 09:49:12 +0800

    version 1.0 可能为最后更新

--- src/main/java/com/mdream/gamemanage/common/tools/Constans.java
+++ src/main/java/com/mdream/gamemanage/common/tools/Constans.java
@@ -6,9 +6,9 @@ public class Constans {
 	public static final String IMG_BASE_SAVE_URL = "http://img.happyplay.net/";
 		
 	//图片存储物理地址
-	//public static final String IMG_BASE_SAVE_PATH = "/upload/img/";
+	public static final String IMG_BASE_SAVE_PATH = "/upload/img/";
 	
-	public static final String IMG_BASE_SAVE_PATH = "F:/img/";
+	//public static final String IMG_BASE_SAVE_PATH = "F:/img/";
 	
 	//游戏资源访问地址
 	public static final String GAME_BASE_SAVE_URL = "http://game.happyplay.net/";
